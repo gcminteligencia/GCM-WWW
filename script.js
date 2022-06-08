bd0cfc60173de641a4a1df09cd823f321af6e59a
Angell Belger <angellbelger@hotmail.com>
2022-06-08 20:57:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-function named(){
-    var width = window.screen.width
-
-    if (width <= 700){
-        
-    }
+function titleFlex(){
+    var name = window.document.querySelector('h1.main')
+    var width = window.document.body.clientWidth
+    
+    if (width <= 700)
+        name.innerHTML = `GCM`
 }
