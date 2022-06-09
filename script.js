8e2ba25b291e2819553e568b5ca9615cebe7e1ac
Angell Belger <angellbelger@hotmail.com>
2022-06-08 21:26:47 -0300

    update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function titleFlex(){
-    var name = window.document.querySelector('h1.main')
+    var name = window.document.querySelector('h1#main')
     var width = window.document.body.clientWidth
     
     if (width <= 700)
