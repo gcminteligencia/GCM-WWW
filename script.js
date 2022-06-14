62cd8ec41b8e22236f6a1db3dc13462bad3ee4ee
Angell Belger <angellbelger@hotmail.com>
2022-06-14 14:30:32 -0300

    update

--- script.js
+++ script.js
@@ -13,8 +13,13 @@ function metadata(){
     var email = window.document.querySelector('input#email')
     var service = window.document.getElementsByName('typeaten')
     var result = window.document.querySelector('div.result')
+    var error = window.document.querySelector('div.error')
 
-    if (name.value.length >= 3 && cpf.value.length >= 11){
+    if (name.value <= 3){
+        window.alert('Atenção, digite um nome válido.')
+    }
+
+    if (name.value.length >= 3 && cpf.value.length >= 11 && service.value != undefined){
         result.innerHTML = `Parabéns Sr(a) ${name.value}, portador do CPF/CNPJ ${cpf.value}, do contato ${contact.value} e do email ${email.value} sua ocorrência foi registrada com sucesso para atender os serviços: ${service.value}. Tenha um bom dia.`
     }else {
         window.alert('Atenção, preencha todos os dados antes de enviar.')
