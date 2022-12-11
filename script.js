32ebc0510e5ce7f74ab7e5ec2e2b0ab7175ab33a
gcminteligencia <gcmdepinteligencia@hotmail.com>
2022-12-11 10:21:49 -0300

    update

--- script.js
+++ script.js
@@ -12,5 +12,7 @@ function formDivisao(){
 
 
 }
+
+
 window.alert(`Hello, ${school.value}!`)
 /* A "Nome da Escola", Solicita palestra da Divisão de Ensino da Guarda Municipal de Altamira, para realizar a palestra sobre "Tema Sugerido" no dia "Ver datas disponíveis", no turno "Manhã/Tarde", com as turmas "Ex: 5° ano, 6°ano". Aguardo retorno no número "(93)xxxx-xxxx", falar com "Nome do requerente"*/
